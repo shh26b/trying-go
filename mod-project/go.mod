0b29bd799f6010843ae474cb41041861ba9af1ff
Shihab Mahamud <shihab4t@gmail.com>
2022-12-06 19:16:45 +0600

    Slices

---
+++ mod-project/go.mod
@@ -0,0 +1,5 @@
+module github.com/shh26b/go-practice/mod-project
+
+go 1.19
+
+require golang.org/x/tour v0.1.0
---
+++ mod-project/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	at_our_exercises "github.com/shh26b/go-practice/mod-project/a_tour_exercises"
+	"golang.org/x/tour/pic"
+)
+
+func main() {
+	pic.Show(at_our_exercises.Pic)
+}
